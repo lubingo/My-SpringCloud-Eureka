fa675bb1130c62d0184ca5b3272fdb7323cedd61
lubing <15810172080@139.com>
2019-05-23 16:44:20 +0800

    Initial commit

---
+++ src/main/java/com/cloud/eureka/Test.java
@@ -0,0 +1,13 @@
+package com.cloud.eureka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+
+    private  static  final Logger logger = LoggerFactory.getLogger(Test.class);
+    public static void main(String[] args) {
+        logger.info("测试日德水电费水电费水电费水电费");
+    }
+}
